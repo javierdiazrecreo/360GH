7b1771fe66d4b5568104778837973093f20bb919
javierdiazrecreo <javier.diaz.recreo@gmail.com>
2025-12-21 15:14:05 -0300

    Add Tuya secrets (temporary)

---
+++ lib/secrets.dart
@@ -0,0 +1,6 @@
+// secrets.dart
+class TuyaSecrets {
+  static const String accessId = "53sspeuskqxsxfcda4x8";
+  static const String accessSecret = "b99e9c80b78045f5ab200a5d4114ac25"; 
+  static const String deviceId = "eb6851ea60756b1e79clcs";
+}
