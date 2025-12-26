44bda1cd49b5e5a66bb54ef8798f31f30ab516af
javierdiazrecreo <javier.diaz.recreo@gmail.com>
2025-12-26 18:26:56 -0300

    Cloud v8

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
-import 'preview_screen.dart';
-
 class CameraScreen extends StatefulWidget {
   final CameraDescription camera;
+
   const CameraScreen({super.key, required this.camera});
 
   @override
@@ -13,67 +12,53 @@ class CameraScreen extends StatefulWidget {
 
 class _CameraScreenState extends State<CameraScreen> {
   late CameraController _controller;
-  bool recording = false;
+  late Future<void> _initFuture;
 
   @override
   void initState() {
     super.initState();
+
     _controller = CameraController(
       widget.camera,
       ResolutionPreset.high,
       enableAudio: true,
     );
-    _controller.initialize().then((_) {
-      if (mounted) setState(() {});
-    });
-  }
 
-  Future<void> record() async {
-    setState(() => recording = true);
-
-    await _controller.startVideoRecording();
-    await Future.delayed(const Duration(seconds: 8));
-    final video = await _controller.stopVideoRecording();
-
-    if (!mounted) return;
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (_) => PreviewScreen(videoPath: video.path),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (!_controller.value.isInitialized) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
-    }
-
-    return Scaffold(
-      body: Stack(
-        children: [
-          CameraPreview(_controller),
-          Positioned(
-            bottom: 40,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: FloatingActionButton(
-                backgroundColor: recording ? Colors.grey : Colors.red,
-                onPressed: recording ? null : record,
-                child: const Icon(Icons.videocam),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    _initFuture = _controller.initialize();
   }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: FutureBuilder(
+        future: _initFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                "Error cámara:\n${snapshot.error}",
+                style: const TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
   }
 }
