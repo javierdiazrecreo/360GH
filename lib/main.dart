525b13ca17ebbacbbad66fd527e244d9c22f400a
javierdiazrecreo <javier.diaz.recreo@gmail.com>
2025-12-26 19:14:30 -0300

    Cloud v11

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+
 import 'config_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   final cameras = await availableCameras();
+
   runApp(MyApp(cameras: cameras));
 }
 
 class MyApp extends StatelessWidget {
   final List<CameraDescription> cameras;
+
   const MyApp({super.key, required this.cameras});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: ConfigScreen(cameras: cameras),
     );
