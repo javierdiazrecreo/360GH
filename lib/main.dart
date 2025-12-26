1ea45f0f6616d290b8e9040222a1881208880235
javierdiazrecreo <javier.diaz.recreo@gmail.com>
2025-12-26 16:29:41 -0300

    Cloud v4

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,27 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'camera_screen.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 🔥 INICIALIZACIÓN DE FIREBASE (OBLIGATORIO)
   await Firebase.initializeApp();
+
   final cameras = await availableCameras();
+
   runApp(MyApp(cameras: cameras));
 }
 
 class MyApp extends StatelessWidget {
   final List<CameraDescription> cameras;
+
   const MyApp({super.key, required this.cameras});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-      ),
+      theme: ThemeData.dark(useMaterial3: true),
       home: CameraScreen(camera: cameras.first),
     );
   }
