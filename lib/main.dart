9a505aa6388db465981fea90f035e3a4b3607a5a
javierdiazrecreo <javier.diaz.recreo@gmail.com>
2025-12-27 19:04:40 -0300

    Cloud v26

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'config_screen.dart';
 
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  List<CameraDescription> cameras = [];
-
-  try {
-    await Firebase.initializeApp();
-  } catch (e) {
-    debugPrint("Firebase init error: $e");
-  }
-
-  try {
-    cameras = await availableCameras();
-    debugPrint("Cameras found: ${cameras.length}");
-  } catch (e) {
-    debugPrint("Camera error: $e");
-  }
-
-  runApp(MyApp(cameras: cameras));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<CameraDescription> cameras;
-  const MyApp({super.key, required this.cameras});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ConfigScreen(cameras: cameras),
+      title: '360 Party',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ConfigScreen(),
     );
   }
 }
