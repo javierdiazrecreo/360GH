7dd438033a57068a3d61fd3a7ebe2c64a6e68a58
javierdiazrecreo <javier.diaz.recreo@gmail.com>
2025-12-27 12:09:46 -0300

    cloud v18

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 import 'config_screen.dart';
 
 late List<CameraDescription> cameras;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 🔥 Inicialización obligatoria de Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // 📷 Carga de cámaras disponibles
   cameras = await availableCameras();
+
   runApp(const MyApp());
 }
 
