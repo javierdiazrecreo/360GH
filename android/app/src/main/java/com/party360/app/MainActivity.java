d7287153cd8e5730e863c6520b1d8c87a1edcab7
javierdiazrecreo <javier.diaz.recreo@gmail.com>
2026-01-02 21:34:23 -0300

    Full v5

---
+++ android/app/src/main/java/com/party360/app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.party360.app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/party360/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.party360.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
